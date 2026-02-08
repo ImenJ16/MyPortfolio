617ebf8b7e5083cd94242f7dae29617bc4f6fb29
ImenJ16 <jouini.imen@esprit.tn>
2026-02-08 22:31:49 +0100

    enhanced the experience section design and added two projects cards

--- obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyPortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+296f32a84799e36ae4eabff1edaa89114710ab68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+389bbc7878fcc159b3de890938313c06f5be69ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyPortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
